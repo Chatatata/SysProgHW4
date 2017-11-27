3e971a4fc3aea5142f77543eb7d7fc0ce11b5475
Buğra Ekuklu <ekuklu@icloud.com>
2017-11-27 03:08:16 +0200

    Deletions.

--- include/address_list.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#include "city.h"
-
-typedef struct city_t address_list_t;
-
-CSVFS_EXPORT int32_t address_list_parse_csv(const char * restrict file_name, address_list_t * restrict addr_list);
